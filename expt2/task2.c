aa6f8845ea09a2089a18a4a659af9390bc991596
Kamran-Shaikh31 <kamranshaikh200031@gmail.com>
2024-12-01 17:16:49 +0530

    Create task2.c

---
+++ expt2/task2.c
@@ -0,0 +1,9 @@
+/*
+    Expt. No : 2 (task 2)
+    Title : WAP to find the sum of all the odd numbers between numbers entered by the
+            user.
+
+    Name: Ansari Huzaifa Javed Ahmed
+    Class & Div: ECS-E
+    UIN: 241S035
+ */
